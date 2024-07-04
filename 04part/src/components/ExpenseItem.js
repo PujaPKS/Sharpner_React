0346a833ec49f08c720b971ce9a6820c91ea61c6
PujaPKS <pkpuja2404@gmail.com>
2024-07-04 14:28:25 +0530

    Writing Complex JSX

---
+++ 04part/src/components/ExpenseItem.js
@@ -0,0 +1,17 @@
+
+function ExpenseItem() {
+  return (
+    <div>
+      <div>15 August 2023</div>
+      
+      <div>
+        <h2>Book</h2>
+        <div> $10</div>
+        <div>Delhi</div>
+      </div>
+      
+    </div>
+  )
+}
+
+export default ExpenseItem
