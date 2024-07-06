cf0b03ef50fd031c803e4ebb39f80d61adf5b2a1
PujaPKS <pkpuja2404@gmail.com>
2024-07-07 01:44:29 +0530

    Creating Wrapper Components

---
+++ 11part/src/components/ExpenseItem.js
@@ -0,0 +1,23 @@
+import Card from './Card'
+import ExpenseDate from './ExpenseDate'
+import './ExpenseItem.css'
+
+function ExpenseItem(props) {
+  
+  return (
+    <Card className='expense-item'>
+      {/* <div>{props.date.toISOString()}</div> */}
+
+      <ExpenseDate date = {props.date}/>
+      
+      <div className='expense-item__location'>{props.location}</div>
+      <div className='expense-item__description'>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'> ${props.price}</div>
+      </div>
+      
+    </Card>
+  )
+}
+
+export default ExpenseItem
